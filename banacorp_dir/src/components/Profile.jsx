f95b85c26ca710e4f489931396e2910cd7c95941
Anders Boyum <Acboyum@gmail.com>
2024-05-22 15:29:38 -0400

    starting to make Profile scaffold

--- banacorp_dir/src/components/Profile.jsx
+++ banacorp_dir/src/components/Profile.jsx
@@ -1,13 +1,37 @@
 import { Link } from "react-router-dom";
 
-const Profile = () => {
-
+const Profile = (props) => {
+    const user = props.user;
     return(
         <>
-            <h3>Hello there</h3>
-            <Link className="nav-link" to="/directory">
-                    Directory
-            </Link>
+            {/* 
+                user.employeeInfo{
+                    id
+: 
+382
+isHR
+: 
+false
+jobRole
+: 
+"QA Engineer"
+location
+: 
+"San Francisco"
+managerID
+: 
+0
+name
+: 
+"Marie Nelson"
+phoneNumber
+: 
+"6028369232"
+salary
+: 
+"67992"
+                }
+            */}
         </>
     )
 }
