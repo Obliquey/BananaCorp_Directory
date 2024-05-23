f992b9156f66ce728ae076c03b35f8adda3d2331
Anders Boyum <Acboyum@gmail.com>
2024-05-23 15:20:49 -0400

    got the predict salary function to work and display properly

--- banacorp_dir/src/components/PredictSalary.jsx
+++ banacorp_dir/src/components/PredictSalary.jsx
@@ -6,52 +6,63 @@ const PredictSalary = () => {
     const [estimatedSalary, setEstimatedSalary] = useState('');
 
     // function to ask model to predict salary
-    // const fetchSalary = async () => {
-    //     const url = //this will be the URL for the prediction model endpoint
-    //     try {
-    //         const response = await fetch(`${url}`).then(red => {
-    //             console.log("What did we get?", res);
-    //         })
-    //     } catch (error) {
+    const fetchSalary = async () => {
+        console.log("Are we in fetch Salary?")
+        const url = "http://localhost:5000/api/predict" //this will be the URL for the prediction model endpoint
+        try {
+            const employee = {
+                // 'employeeInfo.jobRole': `${jobTitle}`,
+                // 'employeeInfo.location': `${location}`
+                employeeInfo:{jobRole: jobTitle, location:location}
+            }
+            const response = await fetch(`${url}`, {
+                method: "POST",
+                headers:{'Content-Type': "application/json"},
+                body: JSON.stringify(employee),
+            });
+            const data = await response.json();
+            console.log("Our data:", Math.round(data[0]));
+            setEstimatedSalary(Math.round(data[0]));
+        } catch (error) {
             
-    //     }
-    // };
-
+        }
+    };
     return (
         <div style={{display:"flex", flexDirection:"column",justifyContent:"center",gap:"12vh", position:"fixed", top:"10vh", left:"50%", transform: "translate(-50%, 0)"}}>
             <h2>Please input your job title and work location</h2>
             {
-                <h4>{estimatedSalary}</h4>
+               estimatedSalary ?  <h4>${estimatedSalary}</h4> : <></>
             }
             <div style={{display:"flex", flexDirection:"row", gap:"4vw", marginLeft:"auto", marginRight:"auto"}}>
                 <div className="dropdown">
-                    <select className="btn dropdown-toggle" style={{backgroundColor:"lightblue"}} value={jobTitle} onChange={e=> setJobTitle(e.target.value)}>
-                        <option class="dropdown-item" value="">Select One...</option>
-                        <option class="dropdown-item" value="Software Engineer">Software Engineer</option>
-                        <option class="dropdown-item" value="Data Scientist">Data Scientist</option>
-                        <option class="dropdown-item" value="Product Manager">Product Manager</option>
-                        <option class="dropdown-item" value="Designer">Designer</option>
-                        <option class="dropdown-item" value="QA Engineer">QA Engineer</option>
-                        <option class="dropdown-item" value="DevOps Engineer">DevOps Engineer</option>
-                        <option class="dropdown-item" value="System Administrator">System Administrator</option>
+                    <select className="btn btn-outline-primary dropdown-toggle" value={jobTitle} onChange={e=> setJobTitle(e.target.value)}>
+                        <option className="dropdown-item" value="">Select One...</option>
+                        <option className="dropdown-item" value="Software Engineer">Software Engineer</option>
+                        <option className="dropdown-item" value="Data Scientist">Data Scientist</option>
+                        <option className="dropdown-item" value="Product Manager">Product Manager</option>
+                        <option className="dropdown-item" value="Designer">Designer</option>
+                        <option className="dropdown-item" value="QA Engineer">QA Engineer</option>
+                        <option className="dropdown-item" value="DevOps Engineer">DevOps Engineer</option>
+                        <option className="dropdown-item" value="System Administrator">System Administrator</option>
                     </select>
                 </div>
                 <div className="dropdown">
-                    <select className="btn dropdown-toggle" style={{backgroundColor:"lightblue"}} value={location} onChange={e=> setLocation(e.target.value)}>
-                        <option class="dropdown-item" value="">Select One...</option>
-                        <option class="dropdown-item" value="New York">New York</option>
-                        <option class="dropdown-item" value="San Francisco">San Francisco</option>
-                        <option class="dropdown-item" value="Los Angeles">Los Angeles</option>
-                        <option class="dropdown-item" value="Chicago">Chicago</option>
-                        <option class="dropdown-item" value="Houston">Houston</option>
-                        <option class="dropdown-item" value="Phoenix">Phoenix</option>
-                        <option class="dropdown-item" value="Philadelphia">Philadelphia</option>
-                        <option class="dropdown-item" value="San Antonio">San Antonio</option>
-                        <option class="dropdown-item" value="San Diego">San Diego</option>
-                        <option class="dropdown-item" value="Dallas">Dallas</option>
+                    <select className="btn btn-outline-primary dropdown-toggle" value={location} onChange={e=> setLocation(e.target.value)}>
+                        <option className="dropdown-item" value="">Select One...</option>
+                        <option className="dropdown-item" value="New York">New York</option>
+                        <option className="dropdown-item" value="San Francisco">San Francisco</option>
+                        <option className="dropdown-item" value="Los Angeles">Los Angeles</option>
+                        <option className="dropdown-item" value="Chicago">Chicago</option>
+                        <option className="dropdown-item" value="Houston">Houston</option>
+                        <option className="dropdown-item" value="Phoenix">Phoenix</option>
+                        <option className="dropdown-item" value="Philadelphia">Philadelphia</option>
+                        <option className="dropdown-item" value="San Antonio">San Antonio</option>
+                        <option className="dropdown-item" value="San Diego">San Diego</option>
+                        <option className="dropdown-item" value="Dallas">Dallas</option>
                     </select>
                 </div>
             </div>
+            <div className="btn btn-primary" style={{width:"40%", marginLeft:"auto", marginRight:"auto"}} onClick={fetchSalary}>Submit</div>
         </div>
     );
 };
--- notebooks/app.py
+++ notebooks/app.py
@@ -1,27 +1,38 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 import pickle
 import pandas as pd
-
+print("In App.py")
 app = Flask(__name__)
-
+CORS(app)
+# app.config['CORS_HEADERS'] = 'Content-Type'
+# CORS(app, resources={r"/*": {"origins": "*"}})
 # Load the model from disk
 with open('linreg_pipeline.pkl', 'rb') as file:
     model = pickle.load(file)
 
 @app.route('/api/predict', methods=['POST'])
+@cross_origin()
 def predict():
+    print("In our flask route")
     # Get the request data
     data = request.get_json(force=True)
-
      # Ensure the data is a list (even if it's just one dictionary)
-    if isinstance(data, dict):
-        data = [data]
+    # if isinstance(data, dict):
+    #     data = [data]
+
+    print("Here's our data?:", data)
+    employeeInfoLocation = data["employeeInfo"]["location"]
+    employeeInfojobRole = data["employeeInfo"]["jobRole"]
 
     # Make a prediction
-    prediction = model.predict(pd.DataFrame(data))
+    prediction = model.predict(pd.DataFrame({'employeeInfo.jobRole':[employeeInfojobRole], 'employeeInfo.location':[employeeInfoLocation]}))
 
     # Return the prediction
-    return jsonify(prediction.tolist())
+    response = jsonify(prediction.tolist())
+    print("OUr response:", response)
+    # response.headers.add('Access-Control-Allow-Origin','*')
+    return response
 
 if __name__ == '__main__':
     app.run(port=5000)
